4dc08eeb32043cbc078fe9502aca92115b4f0de5
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-12-24 09:25:49 +0530

    Create ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,20 @@
+/*
+  217. Contains Duplicate
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int num : nums) {
+            if (hashSet.contains(num)) {
+                return true;
+            }
+            hashSet.add(num);
+        }
+
+        return false;
+
+    }
+}
